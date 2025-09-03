d2804a83ef99c4e0915c23e2f9c509872cc0c97d
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-09-03 23:11:11 +0500

    Bump release version

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -ex
-VERSION=release-4-12-2
+VERSION=release-4-12-4
 
 git clone https://github.com/freeipa/freeipa --single-branch --branch ${VERSION} --depth 1 freeipa.src
 cp -r freeipa.src/* .
